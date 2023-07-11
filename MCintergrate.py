26ed7239c3e66d167cab107fc25e07fe85516139
sguzman24 <136401520+sguzman24@users.noreply.github.com>
2023-07-10 19:01:09 -0700

    Add files via upload

---
+++ MCintergrate.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 17:24:46 2023
+
+@author: seth guzman
+"""
+
+
+def MCint(f, a, b, n):
+    """
+    
+    Parameters
+    ----------
+    f : TYPE function
+        f returns a float between a and b
+    a : TYPE float 
+        DESCRIPTION. lower limit
+    b : TYPE float
+        DESCRIPTION. upper limit
+    n : TYPE int
+        DESCRIPTION, number of random points to take in the box
+
+    Returns
+    -------
+    integral of f from a to b
+
+    """
+
+    from random import random
+    from numpy import linspace 
+    # random returns a random number betwee 0 and 1
+    
+    
+    maxF = .5
+    
+    area = 0
+    saveX = [] 
+    saveY = []
+    for i in range(n):
+        
+        #generate a random point in the boxc
+        #x between a and b
+        #z between 0 and maxF
+        randNoX = random()*(b-a) + a
+        randNoY = random()*maxF
+        saveX.append(randNoX)
+        saveY.append(randNoY)
+        
+        if randNoY <= f(randNoX): area += 1
+        
+    
+    boxArea = (b-a)*maxF
+    integral = area/n * boxArea
+    print(min(saveX), max(saveX))
+    print(min(saveY), max(saveY))
+    return(integral)
+    
+if __name__ == "__main__":
+    
+    #import numpy as np    
+    #import matplotlib.pyplot as plt
+    
+    def f(x) :
+        return x**2
+    
+    area = MCint(f, 0.5 , 2., 500000)
+    
+    print(f"integ = {area: 0.2f}")
